20c5ad53d49c225a452d82ccdc93929050fc3ab7
Andreas Fester <Andreas.Fester@gmx.de>
2019-06-27 13:58:31 +0200

    Fixed printing scaling

--- Examples/src/main/java/afester/javafx/examples/board/PrintPanel.java
+++ Examples/src/main/java/afester/javafx/examples/board/PrintPanel.java
@@ -14,13 +14,13 @@ import javafx.print.Printer.MarginType;
 import javafx.print.PrinterJob;
 import javafx.scene.Group;
 import javafx.scene.Parent;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.scene.transform.Transform;
 import javafx.stage.Stage;
@@ -91,7 +91,7 @@ public class PrintPanel extends BorderPane {
         panZoomView = new Group(pageView);
         pageView.setBackground(new Background(new BackgroundFill(Color.WHITE, new CornerRadii(0), new Insets(0))));
         printDrawingView = new DrawingView(panZoomView);
-//        printDrawingView.setEffect(new DropShadow(2d, 10, 10, Color.GRAY));
+        printDrawingView.setEffect(new DropShadow(2d, 10, 10, Color.GRAY));
         setCenter(printDrawingView);
 
         setupPage(controller.selectedPrinterProperty().get(), controller.selectedPaperProperty().get(), controller.getOrientation()); 
@@ -106,27 +106,92 @@ public class PrintPanel extends BorderPane {
             return;
         }
 
-        System.err.println(printer.getPrinterAttributes().getDefaultPrintResolution()); // 600 dpi
-        // printer.getPrinterAttributes().getSupportedPrintResolutions().forEach(System.err::println);
-
-        // Issue: by definition, one unit in the pageView is 1 mm.
+        // Issue: by definition, in this application, one unit in the pageView is 1 mm.
         // Since this also applies to the preview page, everything fits in the preview 
-        // (the size of the preview pane is directly derived from the width and height of the
+        // (the size of the preview pane is directly derived from the width and height, in mm, of the
         // page layout).
-        // However, when actually printing the page, one unit is NOT 1mm anymore!
+
+        //******************************************************
+        // However, the printing unit is "font points". Not dots or the like!
+        //
+        // > A font is often measured in pt (points). Points dictate the height of the lettering. 
+        // > There are approximately 72 (72.272) points in one inch or 2.54 cm. For example, the 
+        // > font size 72 would be about one inch tall, and 36 would be about a half of an inch.
+        //
+        // 1pt = 1/72 inch = 0,3527777... mm (25,4/72)
+        // 1mm = 2.83464566929pt      (72/25,4)
+        //
+        //       2.83687943262 (experimental value)
+        // 
+        // 1pt = 1/72.272 inch = 0,35145007748 mm (25,4/72.272)
+        // 1mm = 2.84535433071pt     (72.272/25,4)
+
+        final double scale = 72.0/25.4; // factor: mm to points
+
+        // ******************************************************
+
         final double xs = pageView.getScaleX();
         final double ys = pageView.getScaleY();
-
-        pageView.setScaleX(2.8);
-        pageView.setScaleY(2.8);
+        pageView.setScaleX(scale);
+        pageView.setScaleY(scale);
         pageView.setLayoutX(215);
         pageView.setLayoutY(140);
+        
+//        final double x = 400;
+//        final double y = 200;
+//        Pane p = new Pane();
+//        p.setBackground(new Background(new BackgroundFill(Color.LIGHTYELLOW, new CornerRadii(0), new Insets(0))));
+//        
+//        final double screenDPI = Screen.getPrimary().getDpi();
+//        System.err.printf("Screen resolution: %s\n", screenDPI);
+//
+//        final double scaleP = 200.0/70.5;
+//        System.err.printf("Print scale: %s\n", scaleP);
+//
+//        Circle c = new Circle(x, y, 100 * scaleP);
+//        //                          ^^^ UNIT? 
+//        c.setFill(null);
+//        c.setStroke(Color.BLUE);
+//        Line l1 = new Line(x-5, y, x+5, y);
+//        Line l2 = new Line(x, y - 5, x, y + 5);
+//        Circle c2 = new Circle(3, 3, 3);
+//        c2.setFill(null);
+//        c2.setStroke(Color.RED);
+//        
+//        final double x2 = 300;
+//        final double y2 = 50;
+//        Line l21 = new Line(x2-5, y2, x2+5, y2);
+//        l21.setStroke(Color.GREEN);
+//        Line l22 = new Line(x2, y2 - 5, x2, y2 + 5);
+//        l22.setStroke(Color.GREEN);
+//
+//        final double x3 = 500;
+//        final double y3 = 50;
+//        Line l31 = new Line(x3-5, y3, x3+5, y3);
+//        l31.setStroke(Color.GREEN);
+//        Line l32 = new Line(x3, y3 - 5, x3, y3 + 5);
+//        l32.setStroke(Color.GREEN);
+//        
+//        p.getChildren().addAll(c, c2, l1, l2, l21, l22, l31, l32);
+//
+//        System.err.printf("width: %s, height: %s\n", p.getBoundsInParent().getWidth(), p.getBoundsInParent().getHeight());
+//
+//        System.err.printf("Job resolution: %s\n", job.getJobSettings().getPrintResolution());
+
+//        double scaleX
+//            = layout.getPrintableWidth() / p.getBoundsInParent().getWidth();
+//        double scaleY
+//            = layout.getPrintableHeight() / p.getBoundsInParent().getHeight();
+//        System.err.printf("scaleX: %s, scaleY: %s\n", scaleX, scaleY);
+//        p.setScaleX(scaleX);
+//        p.setScaleX(scaleY);
+
         boolean printed = job.printPage(layout, pageView);
         pageView.setScaleX(xs);
         pageView.setScaleY(ys);
         
         if (!printed) {
-            System.err.println("Printing failed!");
+            System.err.println("Printing failed: " + job); // .getJobStatus());
             return;
         }
 
@@ -196,12 +261,12 @@ public class PrintPanel extends BorderPane {
         separator.setStroke(Color.BLUE);
         separator.setStrokeWidth(0.3);
 
-        Rectangle border = new Rectangle(0, 0, printableWidth, printableHeight);
-        border.setFill(null);
-        border.setStroke(Color.BLACK);
-        border.setStrokeWidth(0.4);
+//        Rectangle border = new Rectangle(0, 0, printableWidth, printableHeight);
+//        border.setFill(null);
+//        border.setStroke(Color.BLACK);
+//        border.setStrokeWidth(0.4);
 
-        printContents.getChildren().addAll(separator, topLabel, bottomLabel, topGroup, bottomGroup, border);
+        printContents.getChildren().addAll(separator, topLabel, bottomLabel, topGroup, bottomGroup); // , border);
         pageView.getChildren().add(printContents);
 
         stage.sizeToScene();    // required to properly fit the content to the window
