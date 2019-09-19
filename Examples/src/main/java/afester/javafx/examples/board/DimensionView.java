41b0580b6559d7b0672732855c2472af7ba5f7a0
Andreas Fester <Andreas.Fester@gmx.de>
2019-09-19 12:58:07 +0200

    Fixed dimension lines

--- Examples/src/main/java/afester/javafx/examples/board/DimensionView.java
+++ Examples/src/main/java/afester/javafx/examples/board/DimensionView.java
@@ -2,16 +2,58 @@ package afester.javafx.examples.board;
 
 import javafx.geometry.Bounds;
 import javafx.geometry.Point2D;
-import javafx.geometry.VPos;
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
 import javafx.scene.text.TextBoundsType;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Translate;
+
+
+class TextBox extends Group {
+    private final Text theText;
+
+    public TextBox(String text, Font font) {
+        
+        theText = new Text(text);
+        theText.setFont(font);
+        theText.setBoundsType(TextBoundsType.VISUAL);
+        theText.setFill(Color.DARKRED);
+
+        // value.setTextAlignment(TextAlignment.RIGHT); // In the case of a single line of text, where the width of the node
+                                                        // is determined by the width of the text, the alignment setting has no effect.
+
+// DEBUG: Add a highlighted background behind the text
+//        Bounds b = getBoundsInLocal();
+//        Rectangle r = new Rectangle(b.getMinX(), b.getMinY(), b.getWidth(), b.getHeight());
+//        r.setFill(Color.GREENYELLOW);
+//        r.setStroke(null);
+//        r.setStrokeWidth(0);
+//        getChildren().add(0, r);
+
+// DEBUG: Create a cross indicating the center point
+//        Line l1 = new Line(b.getMinX() + b.getWidth()/2, 
+//                           b.getMinY(), 
+//                           b.getMinX() + b.getWidth()/2, 
+//                           b.getMinY() + b.getHeight());
+//        l1.setStrokeLineCap(StrokeLineCap.BUTT);
+//        l1.setStroke(Color.BLUE);
+//        l1.setStrokeWidth(0.1);
+//
+//        Line l2 = new Line(b.getMinX(),
+//                           b.getMinY() + b.getHeight() / 2, 
+//                           b.getMinX() + b.getWidth(), 
+//                           b.getMinY() + b.getHeight()/2);
+//        l2.setStrokeLineCap(StrokeLineCap.BUTT);
+//        l2.setStroke(Color.RED);
+//        l2.setStrokeWidth(0.1);
+
+        // getChildren().addAll(r, theText, l1, l2);
+        getChildren().addAll(theText);
+    }
+}
 
 public class DimensionView extends Group {
 
@@ -56,67 +98,64 @@ public class DimensionView extends Group {
         //   / * angle
         //   ----->
 
-//        if (angle > 180) {
-//            angle -= 180;
-//        }
-
         Double length = p1.distance(p2);
-        System.err.printf("%s/%s => %s\n", Point2D.ZERO, vecDir, angle);
 
-///////////////////////////
+/////////////////////////
 
         // create the dimension line
         Line l = new Line(p1_1.getX(), p1_1.getY(), p2_1.getX(), p2_1.getY());
         getChildren().add(l);
 
-        // visualize the mid point - for debugging purposes
-        Circle c = new Circle(midpoint.getX(), midpoint.getY(), 0.5);
-        c.setFill(null);
-        c.setStroke(Color.RED);
-        c.setStrokeWidth(0.3);
-        getChildren().add(c);
+//DEBUG: visualize the mid point - for debugging purposes
+//    Circle c = new Circle(midpoint.getX(), midpoint.getY(), 0.5);
+//    c.setFill(null);
+//    c.setStroke(Color.RED);
+//    c.setStrokeWidth(0.3);
+//    getChildren().add(c);
 
+        TextBox tb = new TextBox(String.format("%.1f mm", length), DIM_FONT);
+        final Bounds offset = tb.getLayoutBounds(); // Note: the local X- and Y coordinates of the TextBox are NOT 0!
+        tb.setLayoutX((midpoint.getX() - offset.getMinX()) - offset.getWidth() / 2);
+        tb.setLayoutY(midpoint.getY() - offset.getMinY() - offset.getHeight() / 2);
+
+//        Bounds b = tb.getBoundsInParent();
+//        Circle c4 = new Circle(b.getMinX() + b.getWidth()/2, b.getMinY() + b.getHeight()/2, 0.3);
+//        c4.setFill(null);
+//        c4.setStrokeWidth(0.2);
+//        c4.setStroke(Color.CYAN);
         
-        Group textBox = new Group();
+//        final Bounds size = textBox.localToParent(textBox.getLayoutBounds());
+//
+//        System.err.println("SIZE:" + size);
+//        textBox.setLayoutX(midpoint.getX() - size.getWidth() /2 - 0.2);
+//        textBox.setLayoutY(midpoint.getY() - 2);
+//
+//        final Bounds b2 = textBox.localToParent(textBox.getLayoutBounds());
+//        System.err.println("B2:" + b2);
+
+// DEBUG: 0-based border!!
+//        final Bounds br = tb.getBoundsInLocal();
+//        Rectangle borders = new Rectangle(0, -br.getHeight(), /*br.getMinX(), br.getMinY(), */br.getWidth(), br.getHeight());
+//        borders.setFill(null);
+//        borders.setStroke(Color.RED);
+//        borders.setStrokeWidth(0.1);
+//        borders.getStrokeDashArray().addAll(1.0, 1.0);
+//        tb.getChildren().add(borders);
+
+        // Position the text box at a specific distance above the dimension line 
+        final Point2D p = tb.parentToLocal(midpoint);
+        tb.getTransforms().add(new Rotate(angle, p.getX(), p.getY()));
+        tb.getTransforms().add(new Translate(0,  -2));
         
-        // Add the text for the dimension line
-        //Text value = new Text(midpoint.getX(), midpoint.getY(), String.format("%.1f mm / %s", length, angle));
-        Text value = new Text(String.format("%.1f mm / %s", length, angle));
-        value.setFont(DIM_FONT);
-        value.setBoundsType(TextBoundsType.VISUAL);
-
-        // value.setRotationAxis(new Point3D(midpoint.getX(), midpoint.getY(), 0.0));
-
-        // value.setTextAlignment(TextAlignment.RIGHT); // In the case of a single line of text, where the width of the node
-                                                        // is determined by the width of the text, the alignment setting has no effect.
-
-        value.setTextOrigin(VPos.BASELINE); // the vertical alignment of the text, relative to the given coordinates
-        //value.setRotate(angle);
-
-        Bounds b = value.getBoundsInLocal();
-        Rectangle r = new Rectangle(b.getMinX(), b.getMinY(), b.getWidth(), b.getHeight());
-        r.setFill(Color.GREENYELLOW);
-
-        textBox.getChildren().addAll(r, value);
-        textBox.setRotate(angle);
-        textBox.setLayoutX(midpoint.getX());
-        textBox.setLayoutY(midpoint.getY());
-
-        Bounds b2 = textBox.localToParent(textBox.getLayoutBounds());
-
-        Circle c2 = new Circle(b2.getMinX() + b2.getWidth() / 2.0, b2.getMinY() + b2.getHeight() / 2, 0.5);
-        c2.setFill(null);
-        c2.setStroke(Color.RED);
-        c2.setStrokeWidth(0.3);
-
+//        Circle c2 = new Circle(b2.getMinX() + b2.getWidth() / 2.0, b2.getMinY() + b2.getHeight() / 2, 0.5);
+//        c2.setFill(null);
+//        c2.setStroke(Color.BLUE);
+//        c2.setStrokeWidth(0.3);
+//
 //        Rectangle r2 = new Rectangle(b2.getMinX(), b2.getMinY(), b2.getWidth(), b2.getHeight());
 //        r2.setFill(Color.CADETBLUE);
-        getChildren().addAll(textBox, c2);
+
+        getChildren().addAll(tb); // , c4); // , c2);
     }
 
-//    @Override
-//    public String toString() {
-//        return String.format("HoleView[centerX=%s, centerY=%s, radius=%s, fill=%s, stroke=%s, strokeWidth=%s]",
-//                              getCenterX(), getCenterY(), getRadius(), getFill(), getStroke(),getStrokeWidth());
-//    }
 }
